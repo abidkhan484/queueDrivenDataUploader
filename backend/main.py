2367449642e61aafa82e52ee2231917fefd01203
Abid Khan <abidkhan484@gmail.com>
2023-08-08 10:25:29 +0600

    merge multiple file while upload complete api is called

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, UploadFile, File, Request, Response, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import logging
-from os import getenv
+from os import getenv, listdir
+from os.path import join
 from dotenv import load_dotenv
 from models import UploadData
 
@@ -28,7 +29,7 @@ def read_root():
 
 
 @app.post("/upload/{file_unique_name}", status_code=status.HTTP_201_CREATED)
-def upload_file(file_unique_name: str = "", data: UploadFile = File(...), response: Response = None):
+async def upload_file(file_unique_name: str = "", data: UploadFile = File(...), response: Response = None):
     try:
         chunk_raw_content = data.file.read()
         if (chunk_raw_content):
@@ -44,17 +45,45 @@ def upload_file(file_unique_name: str = "", data: UploadFile = File(...), respon
 
     return {"isSuccess": True, "message": f"Successfully uploaded {file_unique_name} file"}
 
-@app.post("/upload/complete", status_code=status.HTTP_201_CREATED)
-def upload_complete(data: UploadData, response: Response = None):
+async def merge_files(file_unique_name, count):
+    is_success = True
+    if not count:
+        return is_success
+
+    try:
+        file_path = join(getenv("TEMPORARY_FILE_PATH"), file_unique_name)
+        with open(file_path, 'w') as outfile:
+            for idx in range(count):
+                input_file = join(getenv("TEMPORARY_FILE_PATH"), f"{file_unique_name}-"+str(idx))
+                with open(input_file, 'r') as infile:
+                    outfile.write(infile.read())
+    except Exception as e:
+        print(str(e))
+        logging.debug(str(e))
+        is_success = False
+
+    return is_success
+
+@app.post("/upload/complete/", status_code=status.HTTP_201_CREATED)
+async def upload_complete(data: UploadData, response: Response = None):
+    success = {"isSuccess": True, "message": "Successfully uploaded the file"}
+    error = {"message": "There was an error uploading the file"}
     try:
         file_unique_name = data.file_unique_name
-        # write the code to merge the files and delete the chunk files
-        return {"isSuccess": True, "message": f"Successfully uploaded {file_unique_name} file"}
+        file_path = getenv("LOG_PATH")
+        count = len([filename for filename in listdir(file_path) if filename.startswith(file_unique_name)])
+        is_success = await merge_files(file_unique_name, count)
+        if is_success:
+            output = success
+        else:
+            output = error
+            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
     except Exception as e:
         print(str(e))
         logging.debug(str(e))
         response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
-        return {"message": "There was an error uploading the file"}
+        output = error
+    return output
 
 if __name__ == "__main__":
     import uvicorn
--- backend/test_main.py
+++ backend/test_main.py
@@ -1,8 +1,9 @@
 from fastapi.testclient import TestClient
+from fastapi import status
 from dotenv import load_dotenv
 from main import app
 from os import getenv, remove
-from os.path import getsize, exists
+from os.path import getsize, exists, basename
 from math import ceil
 from faker import Faker
 import random
@@ -15,7 +16,7 @@ chunk_size = int(getenv("CHUNK_SIZE"))
 fake = Faker()
 
 def create_temporary_file():
-    file_path = getenv("TEMPORARY_FILE_PATH") + "/" + fake.name() + ".txt"
+    file_path = getenv("TEMPORARY_FILE_PATH") + "/" + fake.name().replace(" ", "-") + ".txt"
 
     with open(file_path, 'w') as file:
         for _ in range(random.randint(100,200)):
@@ -33,16 +34,22 @@ def delete_temporary_file(file_path):
     if exists(file_path):
         remove(file_path)
 
+def delete_chunk_files(file_path, total_files):
+    for i in range(total_files):
+        chunk_file = f"{file_path}-{i}"
+        if exists(chunk_file):
+            remove(chunk_file)
+
 def test_upload_chunk_file():
     file_path = create_temporary_file()
-    fake_name = fake.name()
+    file_name = basename(file_path)
 
     with open(file_path, "rb") as f:
         file_data = f.read()
-    response = client.post(f"/upload/{fake_name}",
+    response = client.post(f"/upload/{file_name}",
                            headers={"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "*", },
                            files={"data": file_data})
-    assert response.status_code == 201
+    assert response.status_code == status.HTTP_201_CREATED
     response_json = response.json()
     assert response_json['isSuccess'] == True
 
@@ -50,22 +57,31 @@ def test_upload_chunk_file():
 
 def test_upload_full_file():
     file_path = create_temporary_file()
-    fake_name = fake.name()
+    file_name = basename(file_path)
     total_files = ceil(getsize(file_path) / chunk_size)
     i = 0
     with open(file_path, "rb") as in_file:
         chunk_file = in_file.read(chunk_size)
         while chunk_file:
-            response = client.post(f"/upload/{fake_name}-{i}",
+            response = client.post(f"/upload/{file_name}-{i}",
                         headers={"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "*", },
                         files={"data": chunk_file})
-            assert response.status_code == 201
+            assert response.status_code == status.HTTP_201_CREATED
             response_json = response.json()
             assert response_json['isSuccess'] == True
             chunk_file = in_file.read(chunk_size)
             i += 1
-    if i == total_files:
-        response = client.post("/upload/complete", json={'file_unique_name': file_path})
-        print(response.text)
 
+    if i == total_files:
+        response = client.post("/upload/complete/", json={'file_unique_name': file_name})
+        assert response.status_code == status.HTTP_201_CREATED
+
+    delete_chunk_files(file_path, total_files)
     delete_temporary_file(file_path)
+
+def test_upload_complete():
+    payload = {"file_unique_name": "example_file_name.txt"}
+    response = client.post("/upload/complete/", json=payload)
+    assert response.status_code == status.HTTP_201_CREATED
+    data = response.json()
+    assert "message" in data
